dea77f6c801286e0cb7a4c7fbc9fce1ee195f4c4
Marcos Orive <marcos@orive.me>
2020-11-11 18:19:05 +0100

    Simplify logger transports. Now it only logs to console.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,13 +4,6 @@ export const logger = createLogger({
     format: format.json(),
     defaultMeta: { service: "user-service" },
     transports: [
-      //
-      // - Write to all logs with level `info` and below to `combined.log`
-      // - Write all logs error (and below) to `error.log`.
-      // - Write logs in simple fortmat to console.
-      //
-      new transports.File({ filename: "../../log/error.log", level: "error" }),
-      new transports.File({ filename: "../../log/all_logs.log" }),
       new transports.Console({format: format.simple()})
     ]
 });
